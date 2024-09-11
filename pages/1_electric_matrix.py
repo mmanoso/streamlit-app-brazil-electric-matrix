72a83b0e79191e37479d89e1541a286212a32040
Mariano Anoso <mariano.anoso@gmail.com>
2024-09-11 09:57:27 -0300

    clean the code of page 1

--- pages/1_electric_matrix.py
+++ pages/1_electric_matrix.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-
-# import geopandas as gpd
 import plotly.express as px
 import streamlit as st
 import visualization_func as vz
@@ -32,13 +30,8 @@ if "filters" not in st.session_state:
 # initialize dataframe in session state for dynamic filtering
 if "df_filtered" not in st.session_state:
     st.session_state.df_filtered = dfData
-# obtain unique values for parameters for filtering data
-# fuel_origin = dfData["fuel_origin"].unique()
-# fuel_type = dfData["fuel_type"].unique()
-# fuel_type_name = dfData["fuel_type_name"].unique()
-# generator_type = dfData["generator_type"].unique()
-# states = dfData["states"].unique()
-# status = dfData["status"].unique()
+
+# define column of interest to group and graph the dataframe
 column_names = ["fuel_origin", "fuel_type", "fuel_type_name", "generator_type"]
 
 # configure the sidebar
@@ -51,6 +44,7 @@ with st.sidebar:
     st.page_link("pages/3_geo_distr.py", label="Geographic Distribution")
 
 st.sidebar.divider()
+
 with st.sidebar:
     # display filters to select
     st.write("Select the filters:")
@@ -77,46 +71,11 @@ with st.sidebar:
             generator_type=st.session_state.filters["generator_type"],
             fuel_type_name=st.session_state.filters["fuel_type_name"],
         )
-# with st.sidebar:
-#     # Filters
-#     # fuel origin
-#     par_multi_fuel_origin = st.multiselect(
-#         "Fuel Origin", options=fuel_origin, default=None
-#     )
-#     # Fuel type
-#     par_multi_fuel_type = st.multiselect("Fuel Type", options=fuel_type, default=None)
-#     # fuel type name
-#     par_multi_fuel_type_name = st.multiselect(
-#         "Fuel Type Name", options=fuel_type_name, default=None
-#     )
-#     # Generator type
-#     par_multi_generator_type = st.multiselect(
-#         "Generator Type", options=generator_type, default=None
-#     )
-#     # states
-#     par_multi_states = st.multiselect("States", options=states, default=None)
-# Status of the plant
-# par_status = st.selectbox("Status", options=status, index=0)
-# Clasification for points in map
-# par_category = st.selectbox("Category", options=column_names, index=0)
-
-# # apply filters to dataframe
-# df_filtered = aux.apply_filters_to_df(
-#     dfData,
-#     status=st.session_state.filters["status"],
-#     states=st.session_state.filters["states"],
-#     fuel_type=st.session_state.filters["fuel_type"],
-#     fuel_origin=st.session_state.filters["fuel_origin"],
-#     generator_type=st.session_state.filters["generator_type"],
-#     fuel_type_name=st.session_state.filters["fuel_type_name"],
-# )
-
 
 # title of the page
 st.header("Brazilian electric matrix - Electric Matrix")
 
-# par_multi_status = st.multiselect("Status", options=status, default=status)
-# par_selec_status = list(st.selectbox("Status", options=status, index=0))
+# select filters to group and graph
 par_multi_category = st.multiselect(
     "Select columns to display in table:", options=column_names, default=column_names
 )
@@ -125,23 +84,15 @@ par_selec_category = st.selectbox(
 )
 # graphs title
 st.subheader(f"Electric Power by {par_selec_category}")
-# apply filters to data
-# df_filtered = aux.apply_filters_to_df(
-#     df=dfData,
-#     status=par_multi_status,
-#     states=par_multi_states,
-#     fuel_origin=par_multi_fuel_origin,
-#     fuel_type=par_multi_fuel_type,
-#     fuel_type_name=par_multi_fuel_type_name,
-#     generator_type=par_multi_generator_type,
-# )
 
+# group dataframe for table and graphs
 df_grouped = aux.groupby_func_to_df(st.session_state.df_filtered, par_multi_category)
 
 # manage colors for graphs
 color_dict = vz.generate_color_dict_plotly(
     categories=dfData[par_selec_category].unique(), colormap="Pastel"
 )
+
 # graphs
 c1, c2 = st.columns(2)
 with c1:
